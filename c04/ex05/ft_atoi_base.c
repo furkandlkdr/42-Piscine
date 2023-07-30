657e4032cd55bf702b7f341a4ddd32d6df8a1466
Furkan Dulkadir <fdulkadi@k1m03s03.42kocaeli.com.tr>
2023-07-30 21:32:14 +0300

    added c04/ex05/temp atoibase

--- c04/ex05/ft_atoi_base.c
+++ c04/ex05/ft_atoi_base.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*   ft_atoi_base.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fdulkadi <@student.42kocaeli.com.tr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/29 15:33:08 by fdulkadi          #+#    #+#             */
-/*   Updated: 2023/07/30 20:37:54 by fdulkadi         ###   ########.fr       */
+/*   Created: 2023/07/30 20:40:34 by fdulkadi          #+#    #+#             */
+/*   Updated: 2023/07/30 21:31:13 by fdulkadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,36 @@ int	ft_isspace(char c)
 	return (0);
 }
 
-int	ft_isdigit(char c)
+int	ft_check(char *str)
 {
-	if (c >= '0' && c <= '9')
-		return (1);
-	return (0);
+	int	i;
+	int	j;
+
+	i = 0;
+	if (ft_strlen(str) <= 1)
+		return (0);
+	while (str[i] != '\0')
+	{
+		if ((str[i] >= 1 && str[i] <= 31) || str[i] == '+' || str[i] == '-')
+			return (0);
+		j = i + 1;
+		while (str[j] <= '\0')
+		{
+			if (str[i] == str[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
 }
 
-int	ft_atoi(char *str)
+int	ft_atoi_base(char *str, char *base)
 {
+	int	turn;
 	int	sign;
-	int	num;
 
-	num = 0;
+	turn = 0;
 	sign = 1;
 	while (ft_isspace(*str))
 		str++;
@@ -42,8 +59,6 @@ int	ft_atoi(char *str)
 	}
 	while (ft_isdigit(*str))
 	{
-		num = num * 10 + (*str - '0');
-		str++;
 	}
-	return (num * sign);
+	return (turn);
 }
