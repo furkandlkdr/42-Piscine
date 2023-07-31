2637e9e73bbe6d45c4857f08c59a89a59ea24c61
Furkan Dulkadir <fdulkadi@k1m20s07.42kocaeli.com.tr>
2023-07-31 16:27:52 +0300

    C03/ex04/ft_strstr.c updated v2

--- c03/ex04/ft_strstr.c
+++ c03/ex04/ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: fdulkadi <@student.42kocaeli.com.tr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/29 13:09:45 by fdulkadi          #+#    #+#             */
-/*   Updated: 2023/07/29 14:26:35 by fdulkadi         ###   ########.fr       */
+/*   Updated: 2023/07/31 16:26:19 by fdulkadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char	*ft_strstr(char *str, char *to_find)
 	int	j;
 
 	i = 0;
-	if (to_find[j] == '\0')
+	if (to_find[i] == '\0')
 		return (str);
 	while (str[i] != '\0')
 	{
